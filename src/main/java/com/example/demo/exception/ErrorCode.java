82e43f686581508f8143ef0390d8847aadc237ea
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-04-03 11:40:40 +0100

    Exception

---
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.example.demo.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+
+
+
+    NO_SUCH_TEACHER_FOUND(2000, "Nie znaleziono nauczyciela o podanym name"),
+    NO_SUCH_STUDENT_FOUND(1000, "Nie znaleziono studenta o podanym name");
+
+
+    private final int code;
+    private final String description;
+
+    ErrorCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
